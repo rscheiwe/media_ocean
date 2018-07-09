f258ff4163a65adb63ca8559f46ed20e6e1b9f0b
rscheiwe <rscheiwe@gmail.com>
2018-07-09 17:29:27 -0400

    Updated Authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:username])
+    if user && user.authenticate(params[:password])
+    
       session[:user_id] = user.id
       redirect_to user
     else
