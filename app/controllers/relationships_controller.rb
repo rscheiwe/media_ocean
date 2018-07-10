4ac7c01d500f9602dc58d1c735954eda89076067
rscheiwe <rscheiwe@gmail.com>
2018-07-09 20:59:01 -0400

    add follower fixed

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,17 @@
 class RelationshipsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @relationship = current_user.active_relationships.build(followed_id: params[:followed_id])
+    if @relationship.save
+      redirect_to user_path(current_user)
+    else
+    end
+  end
+
+  def show
+  end
+
 end
