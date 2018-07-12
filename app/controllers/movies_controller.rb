0ed8fee84e075ee9ec05360f9df6778f61704af6
rscheiwe <rscheiwe@gmail.com>
2018-07-11 22:07:37 -0400

    updated home page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,29 @@
 class MoviesController < ApplicationController
 
   def welcome
+    @movies = nil
+
+    if logged_in?
+
+        response = RestClient.get "https://api.themoviedb.org/3/discover/movie?api_key=8593f125d9ede49fe34d715af7003c5f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=2018-06-15&primary_release_date.lte=2018-08-01"
+        json = JSON.parse(response.body)
+
+          @movies = json["results"].map do |result|
+
+          {
+            title: result["title"],
+            poster_path: result["poster_path"],
+            overview: result["overview"],
+            release_date: result["release_date"],
+            genres: result["genre_ids"].join(", "),
+            apinum: result["id"]
+          }
+
+
+      end
+    else
+      @movies = Movie.all.order("created_at DESC")
+    end
     #mediaocean.com/recent_reviews
   end
 
